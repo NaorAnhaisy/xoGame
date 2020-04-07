ad0329682c7d22dfb955bdfb66d3e26d5bdc9ab8
NaorAnhaisy <naor.anhaisy123@gmail.com>
2020-04-08 02:59:17 +0300

    Updates

---
+++ precache-manifest.3ca949f6245d8d5adbc45881545957f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4de974a1b47dd7f0917a662042a1c19",
+    "url": "/xoGame/index.html"
+  },
+  {
+    "revision": "1a9acd83e1d59825eb09",
+    "url": "/xoGame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96908b720f41cdba2375",
+    "url": "/xoGame/static/js/2.5047d9b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xoGame/static/js/2.5047d9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9acd83e1d59825eb09",
+    "url": "/xoGame/static/js/main.928290cc.chunk.js"
+  },
+  {
+    "revision": "1edfe46737bffb21bbf7",
+    "url": "/xoGame/static/js/runtime-main.35b11cd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xoGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
